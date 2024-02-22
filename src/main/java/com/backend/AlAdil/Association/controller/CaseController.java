13332505e7ab11c4fcdc870158ff8295e516299a
MohannadFathy0 <mohannadfathy123@gmail.com>
2024-02-22 16:33:06 +0200

    new commit

--- src/main/java/com/backend/AlAdil/Association/controller/CaseController.java
+++ src/main/java/com/backend/AlAdil/Association/controller/CaseController.java
@@ -3,11 +3,8 @@ package com.backend.AlAdil.Association.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.backend.AlAdil.Association.dto.CaseDto;
 import com.backend.AlAdil.Association.entity.Case;
@@ -28,5 +25,26 @@ public class CaseController {
 	@PostMapping
 	public Case createCase(@RequestBody Case newCase) {
 		return caseRepository.save(newCase);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Case> updateCase(@PathVariable Long id, @RequestBody Case oldCase) {
+		if (!caseRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+
+		oldCase.setId(id);
+		Case updatedProduct = caseRepository.save(oldCase);
+		return ResponseEntity.ok(updatedProduct);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteCase(@PathVariable Long id) {
+		if (!caseRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+
+		caseRepository.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 }
